7b19229d050416aaa027ae2bf442d668057a9c4c
Nuno Pereira <nunocpereira@outlook.com>
2019-11-26 21:28:07 +0000

    added cart-item

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,6 +6,8 @@ import './checkout.styles.scss'
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+
 const CheckoutPage = ({ cartItems, total }) => {
   return (
     <div className='checkout-page'>
@@ -27,9 +29,7 @@ const CheckoutPage = ({ cartItems, total }) => {
         </div>
       </div>
       {
-        cartItems.map(cartItem =>
-          cartItem.name
-        )
+        cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
       }
       <div className='total'>
         <span>TOTAL: ${total}</span>
