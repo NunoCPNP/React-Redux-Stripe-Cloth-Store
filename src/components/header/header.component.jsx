fa617953d4df204d3d78667c6cbbea6d38e066eb
Nuno Pereira <nunocpereira@outlook.com>
2019-11-29 20:45:32 +0000

    categories

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,7 +11,7 @@ import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import { selectCartHidden } from '../../redux/cart/cart.selectors'
-import { selectCurrentUser } from '../../redux/user/user.selector'
+import { selectCurrentUser } from '../../redux/user/user.selectors'
 
 import './header.styles.scss'
 
